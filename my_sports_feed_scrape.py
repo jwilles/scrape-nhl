96cd955e191515e36414e88473625e20e7afda46
Jamie Hussman <user@Jamies-MacBook-Pro.local>
2017-11-26 20:59:54 -0500

    Player, team and dfs scrapers

--- my_sports_feed_scrape.py
+++ my_sports_feed_scrape.py
@@ -2,9 +2,8 @@ import requests
 import os
 import base64
 
-def send_request():
+def send_request(pull_url):
     # Request
-    pull_url = 'https://api.mysportsfeeds.com/v1.1/pull/nhl/2016-2017-regular/daily_player_stats.json?fordate=20161026&playerstats=G,A,Pts,Sh'
     username = os.environ['sportUser']
     password = os.environ['sportPass']
     
@@ -26,8 +25,35 @@ def send_request():
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
 
+def scrape_dfs(year):
+    allMonths1 = ['10','11','12']
+    allMonths2 = ['01','02','03','04']
+    allDays = ['01','02','03','04','05','06','07','08','09'] + list(range(10,32))
 
+    for month in allMonths1:
+        for day in allDays:
+            pull_url = 'https://api.mysportsfeeds.com/v1.1/pull/nhl/' + str(year) + '-' + str(year+1) + '-regular/daily_dfs.json?fordate=' + str(year) + str(month) + str(day)
+            if __name__ == '__main__':
+                send_request(pull_url)
 
-if __name__ == '__main__':
-  send_request()
+    for month in allMonths2:
+        for day in allDays:
+            pull_url = 'https://api.mysportsfeeds.com/v1.1/pull/nhl/' + str(year) + '-' + str(year+1) + '-regular/daily_dfs.json?fordate=' + str(year+1) + str(month) + str(day)
+            if __name__ == '__main__':
+                send_request(pull_url)
 
+def scrape_teams(year):
+    allTeams = ['bos','cbj','tor','wpj','nyr','det','flo','tbl','mtl','ari','col','min','ott','phi','buf',
+                'wsh','car','njd','stl','pit','nsh','sjs','ana','lak','van','cgy','nyi','chi','edm','dal']
+    for team in allTeams:
+        pull_url = 'https://api.mysportsfeeds.com/v1.1/pull/nhl/' + str(year) + '-' + str(year+1) + '-regular/team_gamelogs.csv?team=' + team
+        if __name__ == '__main__':
+            send_request(pull_url)
+
+def scrape_player(year):
+    allTeams = ['bos','cbj','tor','wpj','nyr','det','flo','tbl','mtl','ari','col','min','ott','phi','buf',
+                'wsh','car','njd','stl','pit','nsh','sjs','ana','lak','van','cgy','nyi','chi','edm','dal']
+    for team in allTeams:
+        pull_url = 'https://api.mysportsfeeds.com/v1.1/pull/nhl/' + str(year) + '-' + str(year+1) + '-regular/player_gamelogs.csv?team=' + team
+        if __name__ == '__main__':
+            send_request(pull_url)
