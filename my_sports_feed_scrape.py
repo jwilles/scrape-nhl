8e25a1cbee4315cd4c834c4f6e9d1b8b4c3111ed
John <johnwilles@gmail.com>
2017-11-28 22:32:05 -0500

    Start parsing pulled json

--- my_sports_feed_scrape.py
+++ my_sports_feed_scrape.py
@@ -1,12 +1,12 @@
 import requests
 import os
 import base64
+import json
 
 def send_request(pull_url):
     # Request
     username = os.environ['sportUser']
     password = os.environ['sportPass']
-    
 
     try:
         response = requests.get(
@@ -18,12 +18,14 @@ def send_request(pull_url):
                 "Authorization": "Basic " + base64.b64encode('{}:{}'.format(username,password).encode('utf-8')).decode('ascii')
             }
         )
-        print('Response HTTP Status Code: {status_code}'.format(
-            status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
+     #   print('Response HTTP Status Code: {status_code}'.format(
+     #       status_code=response.status_code))
+     #   print('Response HTTP Response Body: {content}'.format(
+     #       content=response.content))
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
+
+    return response.content
 
 def scrape_dfs(year):
     allMonths1 = ['10','11','12']
@@ -33,34 +35,34 @@ def scrape_dfs(year):
     for month in allMonths1:
         for day in allDays:
             pull_url = 'https://api.mysportsfeeds.com/v1.1/pull/nhl/' + str(year) + '-' + str(year+1) + '-regular/daily_dfs.json?fordate=' + str(year) + str(month) + str(day)
-            if __name__ == '__main__':
-                send_request(pull_url)
 
     for month in allMonths2:
         for day in allDays:
             pull_url = 'https://api.mysportsfeeds.com/v1.1/pull/nhl/' + str(year) + '-' + str(year+1) + '-regular/daily_dfs.json?fordate=' + str(year+1) + str(month) + str(day)
-            if __name__ == '__main__':
-                send_request(pull_url)
 
 def scrape_teams(year):
     allTeams = ['bos','cbj','tor','wpj','nyr','det','flo','tbl','mtl','ari','col','min','ott','phi','buf',
                 'wsh','car','njd','stl','pit','nsh','sjs','ana','lak','van','cgy','nyi','chi','edm','dal']
     for team in allTeams:
-        pull_url = 'https://api.mysportsfeeds.com/v1.1/pull/nhl/' + str(year) + '-' + str(year+1) + '-regular/team_gamelogs.csv?team=' + team
-        if __name__ == '__main__':
-            send_request(pull_url)
+        pull_url = 'https://api.mysportsfeeds.com/v1.1/pull/nhl/' + str(year) + '-' + str(year+1) + '-regular/team_gamelogs.json?team=' + team
 
 def scrape_player(year):
     allTeams = ['bos','cbj','tor','wpj','nyr','det','flo','tbl','mtl','ari','col','min','ott','phi','buf',
                 'wsh','car','njd','stl','pit','nsh','sjs','ana','lak','van','cgy','nyi','chi','edm','dal']
     for team in allTeams:
-        pull_url = 'https://api.mysportsfeeds.com/v1.1/pull/nhl/' + str(year) + '-' + str(year+1) + '-regular/player_gamelogs.csv?team=' + team
+        pull_url = 'https://api.mysportsfeeds.com/v1.1/pull/nhl/' + str(year) + '-' + str(year+1) + '-regular/player_gamelogs.json?team=' + team
+        response = json.loads(send_request(pull_url))
+       
+        player_game_logs = response['playergamelogs']['gamelogs']
+        for game_log in player_game_logs:
+          print game_log
 
 if __name__ == '__main__':
  
-  host = ''
+  host = 'localhost'
   username ='USERNAME'
   password = 'PASSWORD'
   database = 'DBNAME'
 
-  send_request(pull_url)
+  scrape_player(2016)
+
